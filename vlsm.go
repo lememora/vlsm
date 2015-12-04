81fecca3c86fa9388fa3834a0fc1864b421253ec
Rafael Moraes <contact@moraesrafael.com>
2015-12-03 21:30:17 -0500

    number of subnets

--- vlsm.go
+++ vlsm.go
@@ -4,24 +4,42 @@ import (
   "fmt"
   "log"
   "net"
+  "strconv"
 )
 
 type Subnet struct {
   network net.IPNet
 }
 
-func main() {  
-  fmt.Printf("Enter IPv4 network address in CIDR format (192.168.0.0/16): ")
-  var networkAddress string
-	n, err := fmt.Scanln(&networkAddress)
+func main() {
+  var arg, defaultArg string
+  defaultArg = "192.168.0.0/16" /* default class C */
+  fmt.Printf("Enter IPv4 network address in CIDR format (%s): ", defaultArg)
+	n, err := fmt.Scanln(&arg)
   if n == 0 {
-    networkAddress = "192.168.0.0/16" /* default class C */
+    arg = defaultArg
   } else if err != nil {
     log.Fatal(fmt.Errorf("%s\n", err))
   }
-  ip, net, err := net.ParseCIDR(networkAddress)
+  ip, net, err := net.ParseCIDR(arg)
   if err != nil {
-    log.Fatal(fmt.Errorf("ParseCIDR(%q) = %v, %v", networkAddress, ip, net))
+    log.Fatal(fmt.Errorf("ParseCIDR(%q) = %v, %v", arg, ip, net))
   }
-  fmt.Println(networkAddress)
+
+  defaultArg = "1"
+  fmt.Printf("Enter the number of subnets (%s): ", defaultArg)
+  n, err = fmt.Scanln(&arg)
+  if n == 0 {
+    arg = defaultArg
+  } else if err != nil {
+    log.Fatal(fmt.Errorf("%s\n", err))
+  }
+  subnets, err := strconv.Atoi(arg)
+  if err != nil {
+    log.Fatal(fmt.Errorf("Atoi(%q) = %v", arg, subnets))
+  }
+
+  fmt.Println(ip)
+  fmt.Println(net)
+  fmt.Println(subnets)
 }
